c4d2ffb9c18fea630c6fbf40c641f53337a6961e
pratikkumararyan <104012811+pratikkumararyan@users.noreply.github.com>
2023-03-09 19:05:38 +0530

    Add files via upload

---
+++ scramblewords.js
@@ -0,0 +1,78 @@
+let words = [
+    {
+        word: "unity",
+        hint: "The name of a platform for building games"
+    },
+    {
+        word: "unreal",
+        hint: "The name of a platform for building games"
+    },
+    {
+        word: "scratch",
+        hint: "a platform for starting learning coding"
+    },
+    {
+        word: "python",
+        hint: "a programming language"
+    },
+    {
+        word: "bash",
+        hint: "A programming language"
+    },
+    {
+        word: "basic",
+        hint: "a programming language"
+    },
+    {
+        word: "carbon",
+        hint: "a programming language"
+    },
+    {
+        word: "matlab",
+        hint: "a programming language"
+    },
+    {
+        word: "cuneiform",
+        hint: "A programming language"
+    },
+    {
+        word: "ceemac",
+        hint: "A programming language"
+    },
+    {
+        word: "dynamo",
+        hint: "A programming language"
+    },
+    {
+        word: "elixir",
+        hint: "A programming language"
+    },
+    {
+        word: "focus",
+        hint: "A programming language"
+    },
+    {
+        word: "focal",
+        hint: "A programming language"
+    },
+    {
+        word: "halide",
+        hint: "A programming language"
+    },
+    {
+        word: "jovial",
+        hint: "A programming language"
+    },
+    {
+        word: "kotlin",
+        hint: "A programming language"
+    },
+    {
+        word: "mortran",
+        hint: "A programming language"
+    },
+    {
+        word: "swift",
+        hint: "A programming language"
+    },
+]
---
+++ words.js
@@ -0,0 +1,90 @@
+let wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "ruby",
+        hint: "a programming language"
+    },
+    {
+        word: "matlab",
+        hint: "a programming language"
+    },
+    {
+        word: "php",
+        hint: "a programming language"
+    },
+    {
+        word: "swift",
+        hint: "a programming language"
+    },
+    {
+        word: "rust",
+        hint: "a programming language"
+    },
+    {
+        word: "perl",
+        hint: "programming language"
+    },
+    {
+        word: "scratch",
+        hint: "a platform to start learning coding"
+    },
+    {
+        word: "scala",
+        hint: "a programming language"
+    },
+    {
+        word: "bugs",
+        hint: "related to programming"
+    },
+    {
+        word: "unity",
+        hint: "a widely used platform to make games"
+    },
+    {
+        word: "unreal",
+        hint: "a widely used platform for making games"
+    },
+    {
+        word: "github",
+        hint: "code hosting platform"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "gpu",
+        hint: "computer component"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "mysql",
+        hint: "a relational database system"
+    },
+    {
+        word: "proxy",
+        hint: "related to server application"
+    },
+    {
+        word: "html",
+        hint: "markup language for the web"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    }
+]
